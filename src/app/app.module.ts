b00a6b74ce73708fb9de9f5b0688655413f69217
alexowusu <owusu3y@gmail.com>
2019-12-04 15:37:12 +0000

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientListComponent } from './client-list/client-list.component';
+// import { ClientListComponent } from './client-list/client-list.component';
 import { AddClientComponent } from './add-client/add-client.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { AddProjectComponent } from './add-project/add-project.component';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientListComponent,
+    // ClientListComponent,
     AddClientComponent,
     ProjectListComponent,
     AddProjectComponent,
-    HomeComponent,
+    // HomeComponent,
     SigninComponent,
     CustomersComponent
   ],
