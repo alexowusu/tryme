a1aa028d23e5485799d34f252ddbeb6d0c5c7857
alexowusu <owusu3y@gmail.com>
2019-12-04 18:27:14 +0000

    solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ProjectListComponent } from './project-list/project-list.component';
 import { AddProjectComponent } from './add-project/add-project.component';
 import { SigninComponent } from './signin/signin.component';
 import { CustomersComponent } from './customers/customers.component';
+import { AddCustomersComponent } from './add-customers/add-customers.component';
 
 const routes: Routes = [
  {path: 'customers',component:CustomersComponent},
   {path: 'addclients', component: AddClientComponent},
 {path: 'ProjectList', component: ProjectListComponent},
 {path: 'AddProject', component: AddProjectComponent},
-{path: 'signin', component:SigninComponent}
+{path: 'signin', component:SigninComponent},
+{path:'add-customers',component:AddCustomersComponent}
 ];
 
 @NgModule({
