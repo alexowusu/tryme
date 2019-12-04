c30fc53ce1093ae2229eb618377633901bf99da9
alexowusu <owusu3y@gmail.com>
2019-12-04 15:25:01 +0000

    not

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; 
-import { ClientListComponent } from './client-list/client-list.component';
 import { AddClientComponent } from './add-client/add-client.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { AddProjectComponent } from './add-project/add-project.component';
-import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
+import { CustomersComponent } from './customers/customers.component';
 
 const routes: Routes = [
-  { path : 'clientslist', component : ClientListComponent},
+ {path: 'customers',component:CustomersComponent},
   {path: 'addclients', component: AddClientComponent},
 {path: 'ProjectList', component: ProjectListComponent},
 {path: 'AddProject', component: AddProjectComponent},
-{path:'home', component:HomeComponent},
 {path: 'signin', component:SigninComponent}
 ];
 
