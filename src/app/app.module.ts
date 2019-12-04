6185b669be762e3c59c5a2a99bda1c122b07da4f
alexowusu <owusu3y@gmail.com>
2019-12-04 18:13:17 +0000

    addform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { ClientListComponent } from './client-list/client-list.component';
@@ -29,6 +29,7 @@ import { AddCustomersComponent } from './add-customers/add-customers.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
