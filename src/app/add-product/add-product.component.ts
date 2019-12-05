e1622d14384d7e5dfc991f6a7145ee6815cef777
alexowusu <owusu3y@gmail.com>
2019-12-05 12:26:53 +0000

    new

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-add-product',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+  
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,private service:ProductsService) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+
+      // display form values on success
+      const data = JSON.stringify(this.registerForm.value, null, 4);
+      this.service.postProducts(JSON.parse(data)).subscribe(
+        result => { console.log(result); },
+        error => { console.log(error); }
+      );
+      alert('SUCCESS!! :-)\n\n' + data + '\nAddedd Successfully!');
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
   }
 
 }
